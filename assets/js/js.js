1d9c492c6e8592ede8ffdd91c0ec0668631e26c8
Mateus Monteiro <mateus@mmcriativo.com.br>
2019-12-12 22:39:20 -0300

    Arquivos inicias.  Faltando Finalizar a versão mobile !

---
+++ assets/js/js.js
@@ -0,0 +1,14 @@
+var menu = document.getElementById("menu");
+
+//Open menu
+function opened(){
+    if( menu.style.display =='flex'){
+            
+        menu.style.display ='none';    
+          
+      }else{
+          menu.style.display ='flex';
+      }
+      
+          
+}
